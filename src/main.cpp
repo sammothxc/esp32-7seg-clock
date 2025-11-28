f5eb924dc29b80f9cbee441e5e9cb81a1c2efa57
Sam Warr <sam.warr.tx@gmail.com>
2025-11-27 22:38:43 -0700

    12/24hr time option

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@
 #include <time.h>
 #include <EEPROM.h>
 
-struct WifiConf {
+struct EEPROMConf {
   char wifi_ssid[50];
   char wifi_password[50];
+  bool use12HourFormat;
   char cstr_terminator = 0;
 };
 
@@ -22,7 +23,7 @@ const char* hostname = "ntp-clock";
 IPAddress AP_IP(10,1,1,1);
 IPAddress AP_subnet(255,255,255,0);
 AsyncWebServer server(80);
-WifiConf wifiConf;
+EEPROMConf eepromConf;
 const uint8_t segPins[7] = {1,2,3,4,5,6,7}; // a,b,c,d,e,f,g (cathode pins, drive LOW to turn ON)
 const uint8_t digitPins[4] = {9,10,11,12}; // anode pins (drive HIGH to enable digit)
 const uint8_t colon = 8; // colon pin (drive HIGH to turn ON)
@@ -35,6 +36,8 @@ unsigned long lastTimeUpdate = 0;
 unsigned long lastNTPSync = 0;
 const unsigned long NTP_INTERVAL = 3600000UL;
 uint8_t displayDigits[4] = {8,8,8,8}; // 88:88 digit test
+bool reboot = false;
+unsigned long rebootAt = 0;
 
 // bits 0..6 = a..g
 const uint8_t digits[10] = {
@@ -50,27 +53,27 @@ const uint8_t digits[10] = {
     0b01101111  //9
 };
 
-void readWifiConf();
-void writeWifiConf();
+void readConf();
+void writeConf();
 bool connectToWiFi();
 void setUpAccessPoint();
 void handleWebServerRequest(AsyncWebServerRequest *request);
 void startMDNS();
 void setUpWebServer();
-void setSegmentsFor(uint8_t val);
-void showDigit(uint8_t pos, uint8_t val);
-void updateColon();
-void displayTime();
 void NTPsync();
+void rebootCheck();
+void updateTime();
+void updateColon();
+void display();
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Starting up...");
-    for (uint8_t i=0;i<7;i++){
+    for(uint8_t i=0;i<7;i++){
         pinMode(segPins[i], OUTPUT);
         digitalWrite(segPins[i], HIGH);
     }
-    for (uint8_t i=0;i<4;i++){
+    for(uint8_t i=0;i<4;i++){
         pinMode(digitPins[i], OUTPUT);
         digitalWrite(digitPins[i], LOW);
     }
@@ -79,7 +82,7 @@ void setup() {
     pinMode(led, OUTPUT);
     digitalWrite(led, LOW);
     EEPROM.begin(512);
-    readWifiConf();
+    readConf();
     if(!connectToWiFi()){ setUpAccessPoint(); }
     startMDNS();
     setUpWebServer();
@@ -88,106 +91,42 @@ void setup() {
 }
 
 void loop() {
+    NTPsync();
+    rebootCheck();
+    updateTime();
     updateColon();
+    display();
+}
 
-    if (millis() - lastTimeUpdate >= 1000) {
-        lastTimeUpdate = millis();
-        displayTime();
-    }
+void readConf() {
+    for(size_t i = 0; i < sizeof(eepromConf); i++)
+        ((char*)(&eepromConf))[i] = char(EEPROM.read(i));
+    eepromConf.cstr_terminator = 0;
+}
 
-    for (uint8_t pos=0; pos<4; pos++){
-        showDigit(pos, displayDigits[pos]);
-    }
-
-    if (millis() - lastNTPSync >= NTP_INTERVAL) {
-        lastNTPSync = millis();
-        NTPsync();
+void checkConf() {
+    if(eepromConf.cstr_terminator != 0) {
+        Serial.println("No valid config found, using defaults.");
+        memset(&eepromConf, 0, sizeof(eepromConf));
+        strcpy(eepromConf.wifi_ssid, "your-ssid");
+        strcpy(eepromConf.wifi_password, "your-password");
+        eepromConf.use12HourFormat = false;
+        writeConf();
     }
 }
 
-void setSegmentsFor(uint8_t val) {
-    uint8_t m = digits[val];
-    for (uint8_t s=0;s<7;s++){
-        if (m & (1<<s)) digitalWrite(segPins[s], LOW);
-        else digitalWrite(segPins[s], HIGH);
-    }
-}
-
-void showDigit(uint8_t pos, uint8_t val) {
-    for (uint8_t i=0;i<4;i++) digitalWrite(digitPins[i], LOW); // ensure digits off
-    setSegmentsFor(val); // set segments for value
-    digitalWrite(digitPins[pos], HIGH); // enable digit by driving anode HIGH
-    delayMicroseconds(2200); // ~2.2 ms on time per digit, ~450 Hz refresh for 4 digits
-    digitalWrite(digitPins[pos], LOW); // turn off
-    for (uint8_t s=0;s<7;s++) digitalWrite(segPins[s], HIGH); // turn segments off to avoid ghosting if switching anodes quickly (optional)
-}
-
-void updateColon() {
-    unsigned long now = millis();
-    if (colonOn && now - lastColonChange >= 1000) {
-        colonOn = false;
-        lastColonChange = now;
-        digitalWrite(colon, LOW);
-    }
-    else if (!colonOn && now - lastColonChange >= 1000) {
-        colonOn = true;
-        lastColonChange = now;
-        digitalWrite(colon, HIGH);
-    }
-}
-
-void displayTime() {
-    struct tm timeinfo;
-    if (getLocalTime(&timeinfo)) {
-        int hour = timeinfo.tm_hour;
-        int minute = timeinfo.tm_min;
-
-        displayDigits[0] = hour / 10;
-        displayDigits[1] = hour % 10;
-        displayDigits[2] = minute / 10;
-        displayDigits[3] = minute % 10;
-    }
-}
-
-void NTPsync() {
-    unsigned long startAttempt = millis();
-    struct tm timeinfo;
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
-    Serial.println("NTP sync requested...");
-    if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("WiFi not connected, reconnecting...");
-        WiFi.reconnect();
-        delay(500);
-        return;
-    }
-    while (!getLocalTime(&timeinfo)) {
-        if (millis() - startAttempt > 3000) {
-            Serial.println("NTP sync failed.");
-            return;  // fail gracefully
-        }
-        delay(100);
-    }
-    Serial.println("NTP sync complete!");
-}
-
-void readWifiConf() {
-    for (size_t i = 0; i < sizeof(wifiConf); i++)
-        ((char*)(&wifiConf))[i] = char(EEPROM.read(i));
-    wifiConf.cstr_terminator = 0;
-}
-
-void writeWifiConf() {
-    for (size_t i = 0; i < sizeof(wifiConf); i++)
-        EEPROM.write(i, ((char*)(&wifiConf))[i]);
+void writeConf() {
+    for(size_t i = 0; i < sizeof(eepromConf); i++)
+        EEPROM.write(i, ((char*)(&eepromConf))[i]);
     EEPROM.commit();
 }
 
 bool connectToWiFi() {
-    Serial.printf("Connecting to '%s'\n", wifiConf.wifi_ssid);
+    Serial.printf("Connecting to '%s'\n", eepromConf.wifi_ssid);
     WiFi.mode(WIFI_STA);
     WiFi.setHostname(hostname);
-    WiFi.begin(wifiConf.wifi_ssid, wifiConf.wifi_password);
-    if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+    WiFi.begin(eepromConf.wifi_ssid, eepromConf.wifi_password);
+    if(WiFi.waitForConnectResult() == WL_CONNECTED) {
         Serial.print("Connected. IP: "); Serial.println(WiFi.localIP());
         return true;
     } 
@@ -204,35 +143,57 @@ void setUpAccessPoint() {
 }
 
 void handleWebServerRequest(AsyncWebServerRequest *request) {
-  bool save=false;
-  if(request->hasParam("ssid",true) && request->hasParam("password",true)){
-    String s=request->getParam("ssid",true)->value();
-    String p=request->getParam("password",true)->value();
-    s.toCharArray(wifiConf.wifi_ssid,sizeof(wifiConf.wifi_ssid));
-    p.toCharArray(wifiConf.wifi_password,sizeof(wifiConf.wifi_password));
-    writeWifiConf(); save=true;
-  }
+    bool save=false;
+    if(request->hasParam("ssid",true) && request->hasParam("password",true)){
+        String s=request->getParam("ssid",true)->value();
+        String p=request->getParam("password",true)->value();
+        s.toCharArray(eepromConf.wifi_ssid,sizeof(eepromConf.wifi_ssid));
+        p.toCharArray(eepromConf.wifi_password,sizeof(eepromConf.wifi_password));
+        writeConf(); save=true;
+    }
+    if(request->hasParam("tf", true)) {
+        String tf=request->getParam("tf",true)->value();
+        eepromConf.use12HourFormat = (tf == "12");
+        writeConf(); save=true;
+    }
+    String msg = "<!DOCTYPE html><html><head><title>NTP Clock Config</title></head><body>";
+    if(save){ msg += "<div>Saved! Rebooting...</div>"; } 
+    else{
+        msg += "<h1>NTP Clock Config</h1><form action='/' method='POST'>";
+        msg += "<div>SSID:</div><input type='text' name='ssid' value='"+String(eepromConf.wifi_ssid)+"'/>";
+        msg += "<div>Password:</div><input type='password' name='password' value='"+String(eepromConf.wifi_password)+"'/>";
+        msg += "<div>Time Format:</div>";
+        msg += "<select name='tf'>";
+        msg += eepromConf.use12HourFormat ?
+                "<option value='24'>24-hour</option><option value='12' selected>12-hour</option>" :
+                "<option value='24' selected>24-hour</option><option value='12'>12-hour</option>";
+        msg += "</select>";
+        msg += "<div><input type='submit' value='Save'/></div></form>";
+    }
+    msg += "</body></html>";
 
-  String msg = "<!DOCTYPE html><html><head><title>ESP Wifi Config</title></head><body>";
-  if(save){ msg += "<div>Saved! Rebooting...</div>"; } 
-  else{
-    msg += "<h1>ESP Wifi Config</h1><form action='/wifi' method='POST'>";
-    msg += "<div>SSID:</div><input type='text' name='ssid' value='"+String(wifiConf.wifi_ssid)+"'/>";
-    msg += "<div>Password:</div><input type='password' name='password' value='"+String(wifiConf.wifi_password)+"'/>";
-    msg += "<div><input type='submit' value='Save'/></div></form>";
-  }
-  msg += "</body></html>";
+    request->send(200,"text/html",msg);
+    if(save) {
+        request->client()->close();
+        reboot = true;
+        rebootAt = millis();
+        return;
+    }
+}
 
-  request->send(200,"text/html",msg);
-  if(save){ request->client()->close(); delay(1000); ESP.restart(); }
+void startMDNS() {
+    if(!MDNS.begin(hostname)) { //http://ntp-clock.local
+        Serial.println("Error setting up MDNS responder!");
+        while (1) {
+        delay(1000);
+        }
+    }
+    Serial.println("mDNS responder started");
 }
 
 void setUpWebServer() {
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(200, "text/plain", "Hi! This is NTP Clock :)");
-    });
-    server.on("/wifi", HTTP_GET, handleWebServerRequest);
-    server.on("/wifi", HTTP_POST, handleWebServerRequest);
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){ handleWebServerRequest(request); });
+    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request){ handleWebServerRequest(request); });
     ElegantOTA.setAuth("admin", pass);
     ElegantOTA.begin(&server);
     Serial.println("ElegantOTA server started");
@@ -240,12 +201,80 @@ void setUpWebServer() {
     Serial.println("Web server started");
 }
 
-void startMDNS() {
-    if (!MDNS.begin(hostname)) { //http://ntp-clock.local
-        Serial.println("Error setting up MDNS responder!");
-        while (1) {
-        delay(1000);
+void rebootCheck() {
+    if(reboot && millis() >= rebootAt + 1000) {
+        ESP.restart();
+    }
+}
+
+void NTPsync() {
+    if(millis() - lastNTPSync >= NTP_INTERVAL) {
+        lastNTPSync = millis();
+        unsigned long startAttempt = millis();
+        struct tm timeinfo;
+        Serial.println("NTP sync requested...");
+        if(WiFi.status() != WL_CONNECTED) {
+            Serial.println("WiFi not connected, reconnecting...");
+            WiFi.reconnect();
+            delay(500);
+            return;
+        }
+        while (!getLocalTime(&timeinfo)) {
+            if(millis() - startAttempt > 3000) {
+                Serial.println("NTP sync failed.");
+                return;  // fail gracefully
+            }
+            delay(100);
+        }
+        Serial.println("NTP sync complete!");
+    }
+}
+
+void updateTime() {
+    if(millis() - lastTimeUpdate >= 1000) {
+        lastTimeUpdate = millis();
+        struct tm timeinfo;
+        if(getLocalTime(&timeinfo)) {
+            int hour = timeinfo.tm_hour;
+            int minute = timeinfo.tm_min;
+
+            if(eepromConf.use12HourFormat) {
+                hour = hour % 12;
+                if(hour == 0) hour = 12;  // midnight/noon correction
+            }
+            displayDigits[0] = hour / 10;
+            displayDigits[1] = hour % 10;
+            displayDigits[2] = minute / 10;
+            displayDigits[3] = minute % 10;
         }
     }
-    Serial.println("mDNS responder started");
+}
+
+void updateColon() {
+    unsigned long now = millis();
+    if(colonOn && now - lastColonChange >= 1000) {
+        colonOn = false;
+        lastColonChange = now;
+        digitalWrite(colon, LOW);
+    }
+    else if(!colonOn && now - lastColonChange >= 1000) {
+        colonOn = true;
+        lastColonChange = now;
+        digitalWrite(colon, HIGH);
+    }
+}
+
+void display() {
+    for(uint8_t pos=0; pos<4; pos++) {
+        for(uint8_t i=0;i<4;i++) digitalWrite(digitPins[i], LOW); // ensure digits off
+            uint8_t m = digits[displayDigits[pos]];
+            for(uint8_t s=0;s<7;s++) {
+                if(m & (1<<s)) digitalWrite(segPins[s], LOW);
+                else digitalWrite(segPins[s], HIGH);
+            }
+        digitalWrite(digitPins[pos], HIGH); // enable digit by driving anode HIGH
+        delayMicroseconds(2200); // ~2.2 ms on time per digit, ~450 Hz refresh for 4 digits
+        digitalWrite(digitPins[pos], LOW); // turn off
+        for(uint8_t s=0;s<7;s++) digitalWrite(segPins[s], HIGH); // turn segments off to avoid ghosting if switching anodes quickly (optional)
+    }
 }
