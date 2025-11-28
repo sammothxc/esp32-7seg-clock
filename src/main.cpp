c506427f1a9afd0ac29ad7e81d491b35fa564c58
Sam Warr <sam.warr.tx@gmail.com>
2025-11-27 23:18:04 -0700

    increase eeprom

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <time.h>
 #include <WiFi.h>
 
-#define EEPROM_SIZE 512
+#define EEPROM_SIZE 1024
 #define EEPROM_MAGIC 0xA55A1234
 #define REFRESH 2200 // microseconds per digit
 
@@ -27,6 +27,7 @@ const char* hostname = "ntp-clock";
 const uint8_t segPins[7] = {1,2,3,4,5,6,7}; // a,b,c,d,e,f,g (cathode pins, drive LOW to turn ON)
 const uint8_t digitPins[4] = {9,10,11,12}; // anode pins (drive HIGH to enable digit)
 const uint8_t colon = 8; // colon pin (drive HIGH to turn ON)
+const uint8_t led = 48;
 const unsigned long NTP_INTERVAL = 3600000UL;
 uint8_t displayDigits[4];
 bool colonOn = true;
@@ -78,6 +79,7 @@ void setup() {
         pinMode(digitPins[i], OUTPUT);
         digitalWrite(digitPins[i], LOW);
     }
+    pinMode(led, OUTPUT);
     pinMode(colon, OUTPUT);
     digitalWrite(colon, LOW);
     EEPROM.begin(EEPROM_SIZE);
@@ -87,6 +89,7 @@ void setup() {
     setUpWebServer();
     configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
     NTPsync();
+    digitalWrite(led, LOW);
 }
 
 void loop() {
